c5385bcf53b4c7c3d8b00cda20f19d7b8b3efe6e
emaGET <emanuelepini@getacademy.no>
2026-06-16 14:50:37 +0200

    add, remove, search works

--- GroceryList.15.05/Program.cs
+++ GroceryList.15.05/Program.cs
@@ -2,7 +2,7 @@
 List<string> groceries = [];
 groceries.Add("Cat food");
 groceries.Add("Human food");
-groceries.Add("Milk (for pancakes)");
+groceries.Add("Milk");
 
 // foreach (string item in groceries)
 // {
@@ -19,7 +19,8 @@ while (true)
     }
     
     Console.WriteLine("1 - Add item");
-    Console.WriteLine("2 - Show list");
+    Console.WriteLine("2 - Remove Item");
+    Console.WriteLine("3 - Search");
     Console.WriteLine("Q - Quit");
 
     string input = Console.ReadLine();
@@ -31,6 +32,39 @@ while (true)
         groceries.Add(input);
         // break;
     }
+
+    else if (input == "2")
+    {
+        Console.WriteLine("What would you like to remove?");
+        input = Console.ReadLine();
+        string found = groceries.Find(item => item.ToLower() == input.ToLower());
+        if (groceries.Contains(found))
+        {
+            groceries.Remove(found);
+        }
+        else
+        {
+            Console.WriteLine("Item not found...");
+        }
+    }
+    
+    else if (input == "3")
+    {
+        Console.WriteLine("What are you looking for?");
+        input = Console.ReadLine();
+        string found = groceries.Find(item => item.ToLower() == input.ToLower());
+        
+        if (found != null)
+        {
+            Console.WriteLine("It's on the list!");
+        }
+        else
+        {
+            Console.WriteLine("Item not found... Better if you add it");
+        }
+
+    }
+    
     
     else if (input.ToLower() == "q")
     {
