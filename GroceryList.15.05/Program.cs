cc9a3e32215ffd0992ccbf0d59215fca75a736ae
emaGET <emanuelepini@getacademy.no>
2026-06-16 14:09:21 +0200

    quit option, better structure.

--- GroceryList.15.05/Program.cs
+++ GroceryList.15.05/Program.cs
@@ -1,1 +1,31 @@
-﻿Console.WriteLine("Hello, World!");
+﻿// private bool isRunning = true;
+List<string> groceries = [];
+groceries.Add("Cat food");
+groceries.Add("Human food");
+groceries.Add("Milk (for pancakes)");
+
+// foreach (string item in groceries)
+// {
+//     Console.WriteLine(item);
+// }
+
+while (true)
+{
+    Console.WriteLine("--- Today's grocery list! ---");
+    
+    foreach (string item in groceries)
+    {
+        Console.WriteLine(item);
+    }
+    
+    Console.WriteLine("1 - Add item");
+    Console.WriteLine("2 - Show list");
+    Console.WriteLine("Q - Quit");
+
+    string input = Console.ReadLine();
+
+    if (input.ToLower() == "q")
+    {
+        break;
+    }
+}
